324bac71ba7fd451d48d89228f6a1d8ae767f8d9
Oliver <obsta2010@hotmail.com>
2016-06-08 03:22:10 +0800

    initial push

---
+++ shell.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="shell.css">
+	</head>
+	<div id="wrapper">
+		<body>
+			<div id="resultWrapper">
+			test
+			</div>
+			<div id="promptBox"><div class="promptPS1"> [root @ blastzone] $ </div>
+			<input class="promptInput" autofocus type="text">  </div>
+		</body>	
+	</div>
+</html>
